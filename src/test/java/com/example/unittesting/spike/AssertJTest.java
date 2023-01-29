c695a6989a814967e8a84a03b239aed7684f9d80
fipek <firatipek94@gmail.com>
2023-01-29 22:36:13 +0300

    add AssertJTest

---
+++ src/test/java/com/example/unittesting/spike/AssertJTest.java
@@ -0,0 +1,39 @@
+package com.example.unittesting.spike;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// More readable and include good method
+public class AssertJTest {
+
+    @Test
+    public void learning(){
+        List<Integer> numbers = Arrays.asList(12,15,45);
+
+        //assertThat(numbers, hasSize(3));
+        assertThat(numbers)
+                .hasSize(3)
+                .contains(12,15)
+                .allMatch(x -> x > 10)
+                .allMatch(x -> x < 100)
+                .noneMatch(x -> x < 0);
+
+        assertThat("").isEmpty();
+        assertThat("ABCDE")
+                .contains("BCD")
+                .startsWith("ABC")
+                .endsWith("CDE");
+
+        assertThat("123").containsOnlyDigits();
+        assertThat("A BC").containsWhitespaces();
+
+        //assertThat("", blankString());
+        //assertThat("ABCDE", containsString("BCD"));
+        //assertThat("ABCDE", startsWith("ABC"));
+        //assertThat("ABCDE", endsWith("CDE"));
+    }
+}
