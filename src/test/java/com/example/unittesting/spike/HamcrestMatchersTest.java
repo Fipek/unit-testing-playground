5f88e6c026f456f19c50c8befba7a8472db23f54
fipek <firatipek94@gmail.com>
2023-01-29 22:20:17 +0300

    add hamcrestMatcherTest

---
+++ src/test/java/com/example/unittesting/spike/HamcrestMatchersTest.java
@@ -0,0 +1,28 @@
+package com.example.unittesting.spike;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+// More readable and include good method
+public class HamcrestMatchersTest {
+
+    @Test
+    public void learning(){
+        List<Integer> numbers = Arrays.asList(12,15,45);
+
+        assertThat(numbers, hasSize(3));
+        assertThat(numbers, hasItems(12, 45));
+        assertThat(numbers, everyItem(greaterThan(10)));
+        assertThat(numbers, everyItem(lessThan(100)));
+
+        assertThat("", blankString());
+        assertThat("ABCDE", containsString("BCD"));
+        assertThat("ABCDE", startsWith("ABC"));
+        assertThat("ABCDE", endsWith("CDE"));
+    }
+}
